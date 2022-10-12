57b4cba78aa1a2a2c27b67c12e3288abf9bb32e4
Fabrycio-Nakano <flnalmada@gmail.com>
2022-10-12 18:12:07 -0300

    .

--- scripts/Walking_server.py
+++ scripts/Walking_server.py
@@ -1,10 +1,16 @@
 #! /usr/bin/env python
 
+from humanoid_pequi_control.scripts.caminhada import caminhada
 import rospy
+
+from std_msg.msg import Float64
+from caminhada import caminhada
 
 import actionlib
 
 import humanoid_pequi_control.msg
+
+global current_distance
 
 class WalkingAction(object):
     # create messages that are used to publish feedback/result
@@ -16,38 +22,43 @@ class WalkingAction(object):
         self._as = actionlib.SimpleActionServer(self._action_name, humanoid_pequi_control.msg.WalkingAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
       
+
+    def __callback__(self,data):
+        global current_distance
+        current_distance = data.data
+
     def execute_cb(self, goal):
         # helper variables
         r = rospy.Rate(1)
         success = True
         
-        # append the seeds for the Walking sequence
-        self._feedback.sequence = []
-        self._feedback.sequence.append(0)
-        self._feedback.sequence.append(1)
-        
         # publish info to the console for the user
         rospy.loginfo('%s: Executing, creating Walking sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
         
-        # start executing the action
-        for i in range(1, goal.order):
-            # check that preempt has not been requested by the client
+        rospy.Subscriber("/control/routine", Float64, self.callback)
+
+        diff= current_distance - goal.initial_position
+
+        while diff < goal.distance:
             if self._as.is_preempt_requested():
                 rospy.loginfo('%s: Preempted' % self._action_name)
                 self._as.set_preempted()
                 success = False
                 break
-            self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
-            # publish the feedback
-            self._as.publish_feedback(self._feedback)
-            # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-            r.sleep()
-          
+            caminhada(X0=0,vecGanho1=0,vecGanho2=0,pub=0)
+        
+        success = True
+        
+        
+
         if success:
             self._result.sequence = self._feedback.sequence
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
         
+       
+
+
 if __name__ == '__main__':
     rospy.init_node('Walking')
     server = WalkingAction(rospy.get_name())
